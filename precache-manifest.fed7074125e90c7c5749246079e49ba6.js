523f3603b57c41ea46f02db97e655d240ece3f4f
lesstalkmorecode <edamerdan@gmail.com>
2021-03-29 18:37:05 +0300

    Updates

---
+++ precache-manifest.fed7074125e90c7c5749246079e49ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b947171d218a48c8b6c1da988dad68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d15870c8ef757bb3cdc",
+    "url": "./static/css/main.aca4cc75.chunk.css"
+  },
+  {
+    "revision": "c26bade8caca2e31abc5",
+    "url": "./static/js/2.071c5ef4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.071c5ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d15870c8ef757bb3cdc",
+    "url": "./static/js/main.477f7458.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.477f7458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35b37b1ddde8800026f",
+    "url": "./static/js/runtime-main.92355c6c.js"
+  }
+]);
